ec0715dfe964b30fe464c6cc0fb2b855bbccf557
Charles Roth <charlesrothmates@gmail.com>
2023-12-17 20:01:15 -0500

    Add lapin producer

--- queue-client/src/consumer.rs
+++ queue-client/src/consumer.rs
@@ -1,33 +1,33 @@
 use std::marker::PhantomData;
 use lapin::{
     options::*,
-    types::{FieldTable, AMQPValue},
-    BasicProperties, Channel, Connection, ConnectionProperties,
+    types::FieldTable,
+    Channel
 };
 use anyhow::Result;
 
-use crate::queue_data::{QueueData, QueueClass};
+use crate::queue_data::QueueData;
 
 pub struct Consumer<T: QueueData> {
     channel: Channel,
-    name: String,
+    queue_name: String,
     phantom_data: PhantomData<T>,
 }
 
 impl<T: QueueData> Consumer<T> {
 
     pub async fn new(channel: Channel) -> Result<Self> {
-        let name = T::class().to_string();
+        let queue_name = T::class().to_string();
         // Declare the queue
         channel
-            .queue_declare(name.as_str(), QueueDeclareOptions::default(), FieldTable::default())
+            .queue_declare(&queue_name, QueueDeclareOptions::default(), FieldTable::default())
             .await?;
 
-        Ok(Consumer { channel, name, phantom_data: PhantomData })
+        Ok(Consumer { channel, queue_name, phantom_data: PhantomData })
     }
 
     pub async fn get_next(&self) -> Result<Option<T>> {
-        let delivery = self.channel.basic_get(&self.name, BasicGetOptions::default()).await?;
+        let delivery = self.channel.basic_get(&self.queue_name, BasicGetOptions::default()).await?;
         if let Some(delivery) = delivery {
             Ok(Some(T::deserialize(delivery.data.as_slice())?))
         } else {
---
+++ queue-client/src/producer.rs
@@ -0,0 +1,54 @@
+use std::marker::PhantomData;
+use lapin::{
+    options::*,
+    types::FieldTable,
+    Channel
+};
+use anyhow::Result;
+
+use crate::queue_data::QueueData;
+
+// Your struct representing the producer
+struct Producer<T: QueueData> {
+    channel: Channel,
+    queue_name: String,
+    phantom_data: PhantomData<T>
+}
+
+impl<T: QueueData> Producer<T> {
+    // Create a new producer
+    async fn new(channel: Channel) -> Result<Self> {
+        let queue_name = T::class().to_string();
+        // Declare the exchange
+        channel
+            .queue_declare(&queue_name, QueueDeclareOptions::default(), FieldTable::default())
+            .await?;
+
+        Ok(Producer {
+            channel,
+            queue_name: queue_name,
+            phantom_data: PhantomData
+        })
+    }
+
+    // Publish a single element to the queue
+    async fn publish(&self, message: T) -> Result<()> {
+        let serialized_data = message.serialize()?;
+        self.channel.basic_publish(
+            "",
+            &self.queue_name,
+            BasicPublishOptions::default(),
+            serialized_data.as_slice(),
+            Default::default(),
+        ).await?;
+        Ok(())
+    }
+
+    // Publish a vector of elements to the queue
+    async fn publish_batch(&self, messages: Vec<T>) -> Result<()> {
+        for message in messages {
+            self.publish(message).await?;
+        }
+        Ok(())
+    }
+}
